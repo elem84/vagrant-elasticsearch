ae50410a18bf16c1ac2ce286bdf2b6e3ead01af4
Marcin Lewandowski <elem84@gmail.com>
2019-04-19 17:29:41 +0200

    Zmiana instalatora na Ubuntu 18.04 LTS

--- install.sh
+++ install.sh
@@ -4,19 +4,18 @@ echo "Europe/Warsaw" > /etc/timezone
 dpkg-reconfigure -f noninteractive tzdata
 sudo locale-gen pl_PL.UTF-8
 sudo apt-get update
-sudo apt-get install unzip
-sudo apt-get install apt-transport-https
+sudo apt-get install unzip apt-transport-https
 
 ####################################################################################
 ###################################### JAVA ########################################
 ####################################################################################
 
-sudo apt-get install -y software-properties-common python-software-properties
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get install -y software-properties-common
+echo oracle-java11-installer shared/accepted-oracle-license-v1-2 select true | sudo /usr/bin/debconf-set-selections
+sudo add-apt-repository ppa:linuxuprising/java -y
 sudo apt-get update
-sudo apt-get install oracle-java8-installer
-sudo apt-get install -y oracle-java8-set-default
+sudo apt-get install oracle-java11-installer
+sudo apt-get install -y oracle-java11-set-default
 
 ######################################################################################
 ################################# ELASTIC SEARCH #####################################
@@ -27,19 +26,19 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
 
 sudo apt-get update
-sudo apt-get install -y elasticsearch=6.2.4
+sudo apt-get install -y elasticsearch=6.7.1
 
 printf "network.bind_host: 0\n" >> /etc/elasticsearch/elasticsearch.yml
 printf "network.host: 0.0.0.0\n" >> /etc/elasticsearch/elasticsearch.yml
 printf "http.port: 9200\n" >> /etc/elasticsearch/elasticsearch.yml
-printf "xpack.security.enabled: false\n" >> /etc/elasticsearch/elasticsearch.yml
 
 sudo systemctl daemon-reload
 sudo systemctl enable elasticsearch
 sudo systemctl start elasticsearch
 
-#sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install analysis-stempel
+sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install analysis-stempel
 
+#sudo mkdir /etc/elasticsearch/hunspell/
 #sudo mkdir /etc/elasticsearch/hunspell/pl_PL/
 #cd /etc/elasticsearch/hunspell/pl_PL/
 #sudo wget http://download.services.openoffice.org/contrib/dictionaries/pl_PL.zip
@@ -47,12 +46,12 @@ sudo systemctl start elasticsearch
 #sudo rm -rf pl_PL.zip
 
 sudo systemctl restart elasticsearch
-
-####################################################################################
-##################################### KIBANA #######################################
-####################################################################################
-
-sudo apt-get install -y kibana=6.2.4
+#
+#####################################################################################
+###################################### KIBANA #######################################
+#####################################################################################
+#
+sudo apt-get install -y kibana=6.7.1
 
 sudo systemctl daemon-reload
 sudo systemctl enable kibana
@@ -61,16 +60,16 @@ printf "server.host: \"0.0.0.0\"\n" >> /etc/kibana/kibana.yml
 printf "elasticsearch.url: \"http://localhost:9200\"\n" >> /etc/kibana/kibana.yml
 
 sudo systemctl start kibana
-
-####################################################################################
-################################### LOGSTASH #######################################
-####################################################################################
-
-#sudo apt-get install -y logstash
-#sudo /etc/init.d/logstash start
-
-####################################################################################
-################################# MORFOLOGIK #######################################
-####################################################################################
-
-sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install pl.allegro.tech.elasticsearch.plugin:elasticsearch-analysis-morfologik:6.2.4
+#
+#####################################################################################
+#################################### LOGSTASH #######################################
+#####################################################################################
+#
+##sudo apt-get install -y logstash
+##sudo /etc/init.d/logstash start
+#
+#####################################################################################
+################################## MORFOLOGIK #######################################
+#####################################################################################
+#
+sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install pl.allegro.tech.elasticsearch.plugin:elasticsearch-analysis-morfologik:6.7.1
